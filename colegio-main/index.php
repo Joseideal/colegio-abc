2d508b7f4e4df5f403a69bd043cfc0997b2bab96
Jose Daniel <josealvarez.daniel@gmail.com>
2023-04-09 23:01:26 -0500

    avance página acerca

--- colegio-main/index.php
+++ colegio-main/index.php
@@ -13,35 +13,7 @@
     <script src="javascript/index.js"></script>
 </head>
   <body>
-  <nav class="navbar navbar-expand-sm navbar-light">
-      <div class="container">
-      <a class="navbar-brand" href="#">Colegio abc</a>
-      <button class="navbar-toggler collapse" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="index.php" aria-current="page">Inicio <span class="visually-hidden"> (actual)</span></a>
-          </li>
-          <li class="nav-item"><a class="nav-link" aria-current="page" href="acerca.html">Acerca</a></li>
-          <li class="nav-item"><a class="nav-link" aria-current="page" href="programas.html">Pre-Inscripción</a></li>
-          <li class="nav-item"><a class="nav-link" aria-current="page" href="">Noticias</a></li>
-          <li class="nav-item"><a class="nav-link" aria-current="page" href="contacto.html">Contacto</a></li>
-        </ul>
-        <div class="dropdown">
-          <button onclick="myFunction()" class="dropbtn">Iniciar sesion como:</button>
-            <div id="myDropdown" class="dropdown-content">
-              <a href="ESTUDIANTE.php">ESTUDIANTE</a>
-              <a href="DOCENTE.php">DOCENTE</a>
-              <a href="ACUDIENTE.php">PADRE DE FAMILIA O ACUDIENTE</a>
-              <a href="admin.php">ADMINISTRADOR</a>
-            </div>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <?php include "cabecera.php"; ?>
   
   </header>
       <h1>Colegio de educación integral A-B-C</h1>
